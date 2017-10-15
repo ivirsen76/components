e1bc79286b6043602c7ee3967a69065a35790a8b
Igor Eremeev <eremeev@3cisd.com>
2017-10-14 23:53:53 -0400

    Style nav bar

--- src/Nav.js
+++ src/Nav.js
@@ -9,12 +9,17 @@ export default class Component extends React.Component {
 
     render() {
         return (
-            <div>
-                {this.props.packages.map(item => (
-                    <div key={item.name}>
-                        <Link to={`/${item.name}`}>{item.displayName}</Link>
+            <div className="ui vertical menu" style={{ width: '100%' }}>
+                <div className="item">
+                    <div className="header">Components</div>
+                    <div className="menu">
+                        {this.props.packages.map(item => (
+                            <Link className="item" to={`/${item.name}`}>
+                                {item.displayName}
+                            </Link>
+                        ))}
                     </div>
-                ))}
+                </div>
             </div>
         )
     }
