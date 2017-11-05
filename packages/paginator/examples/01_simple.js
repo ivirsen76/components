e5057f7f2e3d7557b772c6e358f2dd28b2a6e51c
Igor Eremeev <eremeev@3cisd.com>
2017-11-05 00:24:48 -0400

    Update paginator examples

---
+++ packages/paginator/examples/01_simple.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import Paginator from '../src'
+
+/** Simple */
+export default class Component extends React.Component {
+    render() {
+        return <Paginator total={50} />
+    }
+}
--- packages/paginator/examples/02_pageInside.js
+++ packages/paginator/examples/02_pageInside.js
@@ -1,8 +1,9 @@
 import React from 'react'
 import Paginator from '../src'
 
+/** Different current page */
 export default class Component extends React.Component {
     render() {
-        return <Paginator total={50} />
+        return <Paginator total={50} currentPage={3} />
     }
 }
