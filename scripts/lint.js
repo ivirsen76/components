ad1959128a942f076b6e03eed5a9f50d3d5dbca6
Igor Eremeev <eremeev@3cisd.com>
2017-12-15 22:33:35 -0500

    Add lint and test scripts

---
+++ scripts/lint.js
@@ -0,0 +1,22 @@
+#!/usr/bin/env node
+import spawn from 'cross-spawn'
+import { getStagedJsFiles } from './config/utils.js'
+import _includes from 'lodash/includes'
+
+const eslint = require.resolve('eslint/bin/eslint.js')
+const args = process.argv.slice(2)
+
+if (_includes(args, '--staged') || _includes(args, '-s')) {
+    const stagedFiles = getStagedJsFiles()
+    if (stagedFiles.length > 0) {
+        const result = spawn.sync('node', [eslint, '-c', 'ieremeev'].concat(stagedFiles), {
+            stdio: 'inherit',
+        })
+        process.exit(result.status)
+    }
+} else {
+    const result = spawn.sync('node', [eslint, '-c', 'ieremeev', 'src', 'packages'], {
+        stdio: 'inherit',
+    })
+    process.exit(result.status)
+}
---
+++ scripts/test.js
@@ -0,0 +1,52 @@
+#!/usr/bin/env node
+// Set test environment
+process.env.NODE_ENV = 'test'
+
+import spawn from 'cross-spawn'
+import { getStagedJsFiles } from './config/utils.js'
+import config from './config/jestConfig.js'
+import _includes from 'lodash/includes'
+
+const jest = require.resolve('jest/bin/jest.js')
+const args = process.argv.slice(2)
+
+if (_includes(args, '--staged') || _includes(args, '-s')) {
+    let result
+
+    // Get staged files
+    const stagedFiles = getStagedJsFiles()
+    if (stagedFiles.length <= 0) {
+        process.exit()
+    }
+
+    // Get related tests
+    result = spawn.sync(
+        'node',
+        [
+            jest,
+            '--config',
+            JSON.stringify(config),
+            '--listTests',
+            '--json',
+            '--findRelatedTests',
+        ].concat(stagedFiles)
+    )
+    const testFiles = JSON.parse(result.stdout.toString())
+    if (testFiles.length <= 0) {
+        process.exit()
+    }
+
+    result = spawn.sync(
+        'node',
+        [jest, '--config', JSON.stringify(config), '--silent'].concat(testFiles),
+        {
+            stdio: 'inherit',
+        }
+    )
+    process.exit(result.status)
+} else {
+    const result = spawn.sync('node', [jest, '--config', JSON.stringify(config)].concat(args), {
+        stdio: 'inherit',
+    })
+    process.exit(result.status)
+}
