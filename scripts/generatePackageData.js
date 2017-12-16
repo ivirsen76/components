5b5466cee13f5cd83dfd8c141214f1cffde157a4
Igor Eremeev <eremeev@3cisd.com>
2017-12-15 23:07:16 -0500

    Fix lint issues in generate script

--- scripts/generatePackageData.js
+++ scripts/generatePackageData.js
@@ -1,6 +1,5 @@
 import fs from 'fs'
 import path from 'path'
-import chalk from 'chalk'
 import { parse } from 'react-docgen'
 import chokidar from 'chokidar'
 
@@ -9,60 +8,39 @@ const paths = {
     output: path.join(__dirname, '../config/', 'componentData.js'),
 }
 
-const enableWatchMode = process.argv.includes('--watch')
-if (enableWatchMode) {
-    // Regenerate component metadata when components or examples change.
-    chokidar.watch([paths.components]).on('change', function(event, path) {
-        generate(paths)
+function writeFile(filepath, content) {
+    fs.writeFile(filepath, content, err => {
+        err ? console.error(err) : console.info('Component data saved.')
     })
 }
 
-// Generate component metadata
-generate(paths)
-
-function generate(paths) {
-    var errors = []
-    var componentData = getDirectories(paths.components).map(function(componentName) {
-        try {
-            return getComponentData(paths, componentName)
-        } catch (error) {
-            errors.push(
-                'An error occurred while attempting to generate metadata for ' +
-                    componentName +
-                    '. ' +
-                    error
-            )
-        }
-    })
-    var content =
-        '/* eslint-disable */\nexport default ' +
-        JSON.stringify(errors.length ? errors : componentData, null, 4)
-    content = content.replace(/"(require\('[^']*'\).default)"/g, '$1')
-    writeFile(paths.output, content)
+function readFile(filePath) {
+    return fs.readFileSync(filePath, 'utf-8')
 }
 
-function getComponentData(paths, componentName) {
-    const fileName = require.resolve(path.join(paths.components, componentName))
-    const packageJson = require.resolve(path.join(paths.components, componentName, 'package.json'))
-    var content = readFile(fileName)
-    var info = parse(content)
-    return {
-        packageName: JSON.parse(readFile(packageJson)).name,
-        name: componentName,
-        displayName: info.displayName,
-        description: info.description,
-        props: info.props,
-        examples: getExampleData(componentName),
-    }
+function getFiles(filepath) {
+    return fs.readdirSync(filepath).filter(file => fs.statSync(path.join(filepath, file)).isFile())
 }
 
 function getExampleData(componentName) {
+    function getExampleFiles(examplesPath) {
+        let exampleFiles = []
+        try {
+            exampleFiles = getFiles(examplesPath)
+        } catch (error) {
+            console.error(`No examples found for ${componentName}.`)
+        }
+        return exampleFiles
+    }
+
     const examplesPath = path.join(paths.components, componentName, 'examples')
-    var examples = getExampleFiles(examplesPath)
-    return examples.map(function(file) {
-        var filePath = path.join(examplesPath, file)
-        var content = readFile(filePath)
-        var info = parse(content)
+    let examples = getExampleFiles(examplesPath)
+
+    return examples.map(file => {
+        let filePath = path.join(examplesPath, file)
+        let content = readFile(filePath)
+        let info = parse(content)
+
         return {
             filePath,
             title: info.description,
@@ -74,34 +52,46 @@ function getExampleData(componentName) {
     })
 }
 
-function getExampleFiles(examplesPath) {
-    var exampleFiles = []
-    try {
-        exampleFiles = getFiles(examplesPath)
-    } catch (error) {
-        console.log(chalk.red(`No examples found for ${componentName}.`))
+function getComponentData(componentName) {
+    const fileName = require.resolve(path.join(paths.components, componentName))
+    const packageJson = require.resolve(path.join(paths.components, componentName, 'package.json'))
+    let content = readFile(fileName)
+    let info = parse(content)
+    return {
+        packageName: JSON.parse(readFile(packageJson)).name,
+        name: componentName,
+        displayName: info.displayName,
+        description: info.description,
+        props: info.props,
+        examples: getExampleData(componentName),
     }
-    return exampleFiles
 }
 
 function getDirectories(filepath) {
-    return fs.readdirSync(filepath).filter(function(file) {
-        return fs.statSync(path.join(filepath, file)).isDirectory()
+    return fs
+        .readdirSync(filepath)
+        .filter(file => fs.statSync(path.join(filepath, file)).isDirectory())
+}
+
+function generate() {
+    const errors = []
+    let componentData = getDirectories(paths.components).map(componentName =>
+        getComponentData(componentName)
+    )
+    let content =
+        '/* eslint-disable */\nexport default ' +
+        JSON.stringify(errors.length ? errors : componentData, null, 4)
+    content = content.replace(/"(require\('[^']*'\).default)"/g, '$1')
+    writeFile(paths.output, content)
+}
+
+const enableWatchMode = process.argv.includes('--watch')
+if (enableWatchMode) {
+    // Regenerate component metadata when components or examples change.
+    chokidar.watch([paths.components]).on('change', () => {
+        generate()
     })
 }
 
-function getFiles(filepath) {
-    return fs.readdirSync(filepath).filter(function(file) {
-        return fs.statSync(path.join(filepath, file)).isFile()
-    })
-}
-
-function writeFile(filepath, content) {
-    fs.writeFile(filepath, content, function(err) {
-        err ? console.log(chalk.red(err)) : console.log(chalk.green('Component data saved.'))
-    })
-}
-
-function readFile(filePath) {
-    return fs.readFileSync(filePath, 'utf-8')
-}
+// Generate component metadata
+generate()
