834b1dd3aaaa6c718b3a58073d3c6c44698d58c7
Igor Eremeev <eremeev@3cisd.com>
2017-12-17 12:26:30 -0500

    Ignore readme for packages

--- scripts/clean.js
+++ scripts/clean.js
@@ -2,4 +2,4 @@
 const spawn = require('cross-spawn')
 
 const rimraf = require.resolve('rimraf/bin.js')
-spawn.sync(rimraf, ['dist', 'es'], { stdio: 'inherit' })
+spawn.sync(rimraf, ['dist', 'es', 'README.md'], { stdio: 'inherit' })
