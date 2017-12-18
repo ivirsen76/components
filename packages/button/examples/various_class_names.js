421f1f89df18d15e0b1a05ceac53598a9d0f955c
Igor Eremeev <eremeev@3cisd.com>
2017-12-17 23:23:42 -0500

    Replace places

--- packages/button/examples/various_class_names.js
+++ packages/button/examples/various_class_names.js
@@ -6,7 +6,7 @@ export default () => (
         <Button title="Default" />
         <Button className="ui primary compact button" title="Primary" />
         <Button className="ui secondary compact button" title="Secondary" />
-        <Button className="ui red compact button" title="Red" />
         <Button className="ui green compact button" title="Green" />
+        <Button className="ui red compact button" title="Red" />
     </div>
 )
