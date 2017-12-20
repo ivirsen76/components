af83c5df9787166e9e62210473b7736d9cc3134a
Igor Eremeev <eremeev@3cisd.com>
2017-12-20 11:44:18 -0500

    Fix registry address

--- scripts/registry.js
+++ scripts/registry.js
@@ -4,7 +4,7 @@ import spawn from 'cross-spawn'
 const result = spawn.sync('npm', ['config', 'get', 'registry'])
 const registry = result.stdout.toString().trim()
 
-if (registry !== 'https://registry.npmjs.org') {
+if (registry !== 'https://registry.npmjs.org/') {
     console.error('Invalid npm registry')
     process.exit(1)
 }
