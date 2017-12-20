1d67c347dc9fb22fa8b8dac22a140e78333d2506
Igor Eremeev <eremeev@3cisd.com>
2017-12-20 11:42:24 -0500

    Check registry before publishing

---
+++ scripts/registry.js
@@ -0,0 +1,10 @@
+#!/usr/bin/env node
+import spawn from 'cross-spawn'
+
+const result = spawn.sync('npm', ['config', 'get', 'registry'])
+const registry = result.stdout.toString().trim()
+
+if (registry !== 'https://registry.npmjs.org') {
+    console.error('Invalid npm registry')
+    process.exit(1)
+}
