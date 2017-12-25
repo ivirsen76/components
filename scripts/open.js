814d50dd1309d9ad0ddd3f20b9a7cb0f57e0f4d7
Igor Eremeev <eremeev@3cisd.com>
2017-12-24 23:19:38 -0500

    Serve static files using express

--- scripts/open.js
+++ scripts/open.js
@@ -5,12 +5,9 @@ const express = require('express')
 const app = express()
 const buildPath = path.join(__dirname, '..', '/build')
 const port = process.env.PORT || 3000
-console.log(process.env)
 
-if (process.env.NODE_ENV !== 'production') {
-    const staticPath = buildPath
-    app.use(express.static(staticPath))
-}
+const staticPath = buildPath
+app.use(express.static(staticPath))
 app.get('*', (req, res) => {
     res.sendFile(path.join(buildPath, '/index.html'))
 })
