22bfc1ba6bea0ad8ff3d7e39abe18ff11036dcc8
Igor Eremeev <eremeev@3cisd.com>
2018-02-12 13:07:39 -0500

    Revert everything back

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -5,6 +5,26 @@ export default class Component extends React.Component {
         return (
             <div>
                 <h1>About</h1>
+                <p>My name is Igor Eremeev.</p>
+                <p>This site is the demo of some packages which I&apos;ve created.</p>
+                <p>
+                    It&apos;s not only the demo site, it&apos;s also the development environment,
+                    which I use to create components. It includes:
+                </p>
+                <ul>
+                    <li>Generator to create first component structure</li>
+                    <li>Automatic documentation using comments inside code</li>
+                    <li>Hot reloading to see the updates in a browser immediately</li>
+                    <li>Automatic testing for examples using Jest snapshot</li>
+                    <li>Build scripts to precompile the code before publishing</li>
+                    <li>Auto generated readme file</li>
+                </ul>
+
+                <p>
+                    <a href="https://github.com/ivirsen76/components">
+                        <i className="github icon" />Source code
+                    </a>
+                </p>
             </div>
         )
     }
