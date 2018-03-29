a0871dcf7674ccb1b7f39ee472744a92349a1b6b
Igor Eremeev <eremeev@3cisd.com>
2018-03-28 23:33:13 -0400

    Update jestConfig

--- scripts/config/jestConfig.js
+++ scripts/config/jestConfig.js
@@ -2,16 +2,14 @@ export default {
     setupFiles: [require.resolve('babel-polyfill'), require.resolve('./jestSetup.js')],
     snapshotSerializers: ['enzyme-to-json/serializer'],
     moduleNameMapper: {
-        '^.+\\.(css|scss|less|mp3)$': 'identity-obj-proxy',
-        '^@ieremeev/(.*)': '<rootDir>/packages/$1/src/index.js',
+        '^.+\\.(css|scss|less|mp3|svg)$': 'identity-obj-proxy',
         '^react$': '<rootDir>/node_modules/react',
         '^react-dom$': '<rootDir>/node_modules/react-dom',
     },
     roots: ['<rootDir>/src', '<rootDir>/packages', '<rootDir>/scripts'],
     testMatch: ['**/?(*).test.js'],
-    testPathIgnorePatterns: ['/node_modules/', '/coverage/'],
+    testPathIgnorePatterns: ['/node_modules/', '/coverage/', '/es/', '/dist/'],
     transform: {
-        '^.+\\.(js|jsx)$': require.resolve('./babelTransform.js'),
+        '^.+\\.js$': require.resolve('./babelTransform.js'),
     },
-    transformIgnorePatterns: ['/(?!node_modules/cccisd-)node_modules/'],
 }
