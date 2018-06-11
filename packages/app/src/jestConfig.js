f28f0859d159389c3931eefa9964ad8ce1eeb044
Igor Eremeev <eremeev@3cisd.com>
2018-06-11 02:01:43 -0400

    Find module based on src/client

--- packages/app/src/jestConfig.js
+++ packages/app/src/jestConfig.js
@@ -1,12 +1,13 @@
 module.exports = {
     setupFiles: [require.resolve('babel-polyfill')],
     snapshotSerializers: [require.resolve('./serializer.js')],
+    moduleDirectories: ['node_modules', 'src/client'],
     moduleNameMapper: {
         '^.+\\.(css|scss|less|mp3)$': 'identity-obj-proxy',
         '^react$': '<rootDir>/node_modules/react',
         '^react-dom$': '<rootDir>/node_modules/react-dom',
     },
-    roots: ['<rootDir>/src/client'],
+    roots: ['<rootDir>/src'],
     testMatch: ['**/?(*.)test.js'],
     testPathIgnorePatterns: ['/node_modules/', '/coverage/'],
     transform: {
