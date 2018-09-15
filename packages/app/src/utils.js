0b8999fdb669bfc22fa09490b31b95071011e18c
Igor Eremeev <eremeev@3cisd.com>
2018-09-14 20:22:30 -0400

    Stringify env variables

--- packages/app/src/utils.js
+++ packages/app/src/utils.js
@@ -14,7 +14,7 @@ module.exports = {
         return Object.keys(process.env)
             .filter(key => /^IE_/.test(key))
             .reduce((env, key) => {
-                env[`process.env.${key}`] = process.env[key]
+                env[`process.env.${key}`] = JSON.stringify(process.env[key])
                 return env
             }, {})
     },
