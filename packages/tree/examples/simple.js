ac1308d9226ddb58087045fc7bfe622e40e65243
Igor Eremeev <eremeev@3cisd.com>
2018-09-19 20:47:49 -0400

    Add example

---
+++ packages/tree/examples/simple.js
@@ -0,0 +1,38 @@
+import React from 'react'
+import Tree from '@ieremeev/tree'
+
+export default () => {
+    const Component = (
+        <div className="ui message">
+            Content
+        </div>
+    )
+    const data = {
+        id: 1,
+        children: [
+            { id: 2, component: Component },
+            {
+                id: 4,
+                component: Component,
+                isAdult: true,
+                children: [
+                    { id: 5, component: Component },
+                    { id: 6, component: Component },
+                    {
+                        id: 8,
+                        component: Component,
+                        isAdult: true,
+                        children: [
+                            { id: 9, component: Component },
+                            { id: 10, component: Component },
+                        ],
+                    },
+                ],
+            },
+            { id: 3, component: Component },
+            { id: 7, component: Component },
+        ],
+    }
+
+    return <Tree tree={data} dragDropType="BUILDER" onDrop={() => {}} />
+}
