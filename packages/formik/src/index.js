3008b239e0fbbc7a2cd20afb8ee54e98e5d5152e
Igor Eremeev <eremeev@3cisd.com>
2018-09-25 19:39:01 -0400

    Implement formik wrapper logic

--- packages/formik/src/index.js
+++ packages/formik/src/index.js
@@ -1,10 +1,34 @@
 import React from 'react'
+import PropTypes from 'prop-types'
+import * as original from 'formik'
+import Sync from './sync.js'
+
+const { Formik } = original
 
 /** Wrapper for Formik package */
-export default class Formik extends React.Component {
-    render() {
-        return (
-            <div>Content</div>
-        )
+class FormikWrapper extends React.Component {
+    static propTypes = {
+        onValidChange: PropTypes.func,
+        render: PropTypes.func,
     }
+
+    render() {
+        if (!this.props.onValidChange || !this.props.render) {
+            return <Formik {...this.props} />
+        }
+
+        const render = args => (
+            <div>
+                <Sync {...args} update={this.props.onValidChange} />
+                {this.props.render(args)}
+            </div>
+        )
+
+        return <Formik {...this.props} render={render} />
+    }
+}
+
+module.exports = {
+    ...original,
+    Formik: FormikWrapper,
 }
