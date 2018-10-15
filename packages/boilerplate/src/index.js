3058118fdda7da67ce547000d17bb374a7e7c427
Igor Eremeev <eremeev@3cisd.com>
2018-10-15 15:42:17 -0400

    Add sentry tracking

--- packages/boilerplate/src/index.js
+++ packages/boilerplate/src/index.js
@@ -1,3 +1,11 @@
+import { init } from '@sentry/browser'
+
+if (process.env.IE_SENTRY_DSN) {
+    init({
+        dsn: process.env.IE_SENTRY_DSN,
+    })
+}
+
 export { default as App } from './components/App'
 export { default as Route } from './components/Route'
 export { default as NotFound } from './components/NotFound'
