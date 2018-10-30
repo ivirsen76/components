dc7c41f218783f8f749a4a1906f28451dead02bb
Igor Eremeev <eremeev@3cisd.com>
2018-10-30 09:41:44 -0400

    Set flag that sentry is working

--- packages/boilerplate/src/index.js
+++ packages/boilerplate/src/index.js
@@ -1,9 +1,11 @@
 import { init } from '@sentry/browser'
+import _set from 'lodash/set'
 
 if (process.env.IE_SENTRY_DSN) {
     init({
         dsn: process.env.IE_SENTRY_DSN,
     })
+    _set(window, 'ieremeev.sentry', true)
 }
 
 export { default as App } from './components/App'
