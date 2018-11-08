50edfd9ed21f5fe69109c795b6d480af7312ec22
Igor Eremeev <eremeev@3cisd.com>
2018-11-08 12:14:43 -0500

    Add icons package

---
+++ packages/icons/README.md
@@ -0,0 +1,3 @@
+# @ieremeev/icons
+
+[Documentation](http://demo.igor-eremeev.com/components/icons)
---
+++ packages/icons/src/index.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+/** SVG icons */
+export default class Icons extends React.Component {
+    render() {
+        return (
+            <div>Content</div>
+        )
+    }
+}
