7126b9cf40d4d47a13ea68e09015712687ce8cf5
Igor Eremeev <eremeev@3cisd.com>
2019-01-21 08:48:51 -0500

    Use cdn url if necessary

--- packages/boilerplate/src/setup.js
+++ packages/boilerplate/src/setup.js
@@ -1,4 +1,6 @@
 // Sets up public path for webpack
 // eslint-disable-next-line
 __webpack_public_path__ =
-    process.env.WEBPACK_PUBLIC_PATH || window.location.protocol + '//' + window.location.host + '/'
+    process.env.WEBPACK_PUBLIC_PATH ||
+    process.env.IE_CDN_URL ||
+    window.location.protocol + '//' + window.location.host + '/'
